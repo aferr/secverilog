52f52f82fee060c52ec9161f4fae65e17179ca92
dz333 <dzag@cs.cornell.edu>
2022-02-01 16:05:27 -0500

    added test illustrating the sad thing we don't want to happen but is totally allowed by the definition of the label

--- test/itelbl.v
+++ test/itelbl.v
@@ -8,9 +8,10 @@ module labelprop(
    reg                 seq {L} final_data;
    reg                 seq {L} old_lbl;
    wire 	       seq {L} isMisspec;
-   reg                 seq {L} tmp_data_tag;   
+   reg                 seq {L} tmp_data_tag;
    reg 		       seq {SPEC isMisspec, old_lbl} tmp_data;   
-
+   reg                 seq {LH old_lbl} down;
+   
    //begin all should succeed
    always@(*) begin     
       isMisspec = (l_valid && (l_status == tmp_data_tag)) ? 1 : 0;      
@@ -22,10 +23,12 @@ module labelprop(
    always@(posedge clk) begin
       if (isMisspec) begin
 	 tmp_data <= 0;
-	 tmp_data_tag <= 0;	 
+	 tmp_data_tag <= 0;
+	 down <= down;	 
       end else begin
-	 tmp_data <= tmp_data;
-	 tmp_data_tag <= tmp_data_tag;	 
+	 tmp_data <= tmp_data;	 
+	 tmp_data_tag <= tmp_data_tag;
+	 down <= tmp_data; //this isn't what we want, but it makes sense w/ the type system	  
       end
    end
 
