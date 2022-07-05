6ca7714003f4f7ea5d3125d7bd6792abb69a1a2c
dz333 <dzag@cs.cornell.edu>
2022-07-05 16:44:10 -0400

    figured out how to encode labels that track things right\!

--- test/policy.fun
+++ test/policy.fun
@@ -1,5 +1,15 @@
 (declare-fun epol (Int) Bool)
 
 (assert (= (epol 0) false))
-(assert (= (epol 1) true))
-;(assert (forall ((x Int)) (implies (not (= x 0)) (= (epol x) True))))
+(assert (forall ((x Int)) (implies (not (= x 0)) (epol x) )))
+
+(declare-fun miss (Int Int Int) Bool)
+(assert (forall ((x Int) (y Int) (z Int))
+	(ite (and (not (= x 0)) (<= y z)) (miss x y z) (not (miss x y z)))
+	))
+
+(declare-fun Domain (Int) Label)
+(assert
+	(forall ((x Int) (y Int)) (implies (<= x y) (leq (Domain x) (Domain y)))))
+; Bottom bit (i.e., even integers) is the valid bit, if its zero then treat as TOP
+(assert (forall ((x Int)) (implies (= (mod x 2) 0) (= HIGH (Domain x)))))
--- test/policy.v
+++ test/policy.v
@@ -1,15 +1,18 @@
 module policy(
-	      input clk,
-	      input rst,
-	      input invalid,
-	      output {H} out
+	      input 	  clk,
+	      input 	  rst,
+	      input 	  invalid,
+	      input [4:0] missId,
+	      input [3:0] newSpecId,
+	      input 	  newSpecValid,	      
+	      output 	  {H} out
 );
 
 
    reg 		    seq {erase(L;epol invalid;();H)} data_reg;
    reg 		    seq {erase(L;epol invalid;();H)} dreg_2;   
 
-
+   
    always@(posedge clk) begin
       if (rst) data_reg <= 0;
       else data_reg <= data_reg; //should fail because policy may be true this cycle and become false next cycle
@@ -21,5 +24,17 @@ module policy(
    end
 
    assign out = (invalid) ? data_reg : 0;
+
+   reg [4:0] seq {erase(Domain spec;miss invalid, missId; spec;H)} spec;
+
+   wire      com {H} inv = (invalid && missId <= spec) ? 1 : 0;   
    
+   always@(posedge clk) begin
+      if (rst)
+	spec <= 0;
+      else if (inv)     
+       	spec <= 0; //should succeed since this resets it to a HIGH label for next cycle
+      else
+	spec <= spec; //should succeed since we've proven it not invalid
+   end
 endmodule
